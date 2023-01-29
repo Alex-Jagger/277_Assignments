544a300e4fe2d84b8c218744dee63f7d4dd5f8f2
lxcleo <li.7494@osu.edu>
2023-01-28 17:48:09 -0800

    Updated sys_id function

--- lib/sys_id.m
+++ lib/sys_id.m
@@ -6,6 +6,22 @@
 %   sys: Fitted system
 %   kapa: System gain
 %   tau: System time constant
-function sys_id()
+function [system,kapa,tau] = sys_id(name,Tss)
+load(name);
+time = data{2}.Values.Time;
+response = data{2}.Values.Data;
+step = data{1}.Values.Data;
+
+index_temp = time >= 10;
+time = time(index_temp);
+response = response(index_temp);
+step = step(index_temp);
+data= iddata(response, step,Tss);
+tau_init = 42076.6;
+kapa_init = 79.0012;
+sys_init = idtf(tau_init, [kapa_init 1 0]);
+system = tfest(data, sys_init);
+kapa = system.Numerator;
+tau = system.Denominator(2);
 
 end
--- src/system_id/system_id.m
+++ src/system_id/system_id.m
@@ -6,5 +6,5 @@ Tss = param.encoder.Tss;
 stepsize = 0.3;
 chirp_init_freq = 0; %Hz
 chirp_target_freq = 100;
-[sys,kapa,tau] = get_tf('system_id\step_3_1.mat',Tss);
+[sys,kapa,tau] = sys_id('system_id\step_3_1.mat',Tss);
 
