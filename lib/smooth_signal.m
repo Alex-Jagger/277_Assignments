46428d625cacb9aceb7c0d13bfeef6f1fc4efece
Alex-Jagger <alex87@ucla.edu>
2023-02-20 22:56:15 -0800

    added hardware test for SOFCIO

--- lib/smooth_signal.m
+++ lib/smooth_signal.m
@@ -1,8 +1,9 @@
 function [signal_smooth, time_out] = smooth_signal(signal, time, n)
+    n = floor(n/2)*2;
     signal_smooth = zeros(length(signal) - n + 1, n);
     for ii = 1:n
         signal_smooth(:, ii) = signal(ii:end - n + ii);
     end
     signal_smooth = mean(signal_smooth, 2);
-    time_out = time(1:end - n + 1);
+    time_out = time(1 + n/2:end - n/2 + 1);
 end
