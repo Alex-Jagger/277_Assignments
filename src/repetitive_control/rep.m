2147b1b4a23dc2e1947ca7cfb98d1516716e35a1
lxcleo <li.7494@osu.edu>
2023-03-03 22:12:59 -0800

    Created new folder for repetitive control

---
+++ src/repetitive_control/rep.m
@@ -0,0 +1,12 @@
+clear; close all; clc
+%%
+init
+%% System ID
+% 
+load_our_parameters
+load_hardware_parameters
+%%%%%%%%%%  TODO %%%%%%%%%%%%%%%
+% 1. System ID, find wn and zeta
+% 2. Find poles of the system
+% 3. Design controller F
+%% 
